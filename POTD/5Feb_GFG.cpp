6ef1e792e7d44950ff145de2dc27729a282cd0ea
hey-RAJ-here <priyanshuraj1904@gmail.com>
2026-02-05 12:48:36 +0530

    Maximize Number of 1's

---
+++ POTD/5Feb_GFG.cpp
@@ -0,0 +1,19 @@
+//Maximize Number of 1's
+class Solution {
+public:
+    int maxOnes(vector<int>& arr, int k) {
+         int left = 0, zeros = 0, ans = 0;
+
+    for (int right = 0; right < arr.size(); right++) {
+        if (arr[right] == 0) zeros++;
+
+        while (zeros > k) {
+            if (arr[left] == 0) zeros--;
+            left++;
+        }
+
+        ans = max(ans, right - left + 1);
+    }
+    return ans;
+    }
+};
